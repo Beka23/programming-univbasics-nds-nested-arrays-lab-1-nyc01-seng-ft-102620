343291a04e1ec8c60f6fbd644d564951e1fa9574
Beksultan <beksultankaiypov@gmail.com>
2020-10-13 19:28:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,7 +44,7 @@ def matrix_update(matrix, row, column, new_value)
   # Return the updated matrix
  matrix[row][column]
  matrix[row][column] = matrix[new_value]
- matrix[row][column]
+ matrix[new_value]
 
  
 end
